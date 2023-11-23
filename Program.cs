248e7b10d5bbfdd5a09194e897c7ac61e05b4e9c
23-24 <23-24@PC11>
2023-11-23 11:16:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace fhahgh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int c;
+
+            Console.WriteLine("molq vuvedete stoinost na b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("molq vuvedete stoinost na b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("molq vuvedete stoinost na a");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("molq vuvedete stoinost na c");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            int D = (b * b - 4 * a * c);
+            Console.WriteLine("diskriminaciqta e: " + D + "%");
+
+            if (D <= 0)
+            {
+                Console.WriteLine("nqma koreni");
+            }
+            else
+            {
+                Console.WriteLine("ima koreni");
+            }
+               
+        }
+    }
+}
